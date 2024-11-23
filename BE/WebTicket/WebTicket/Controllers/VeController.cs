6b071dfd058f0caa9d0d685d2bb8987851ff2a50
vinhdn20 <vinhdn20@gmail.com>
2024-11-23 10:03:04 +0700

    add api delete, update api xuatve

--- BE/WebTicket/WebTicket/Controllers/VeController.cs
+++ BE/WebTicket/WebTicket/Controllers/VeController.cs
@@ -1,5 +1,4 @@
 ﻿// Import required modules
-using Castle.Core.Resource;
 using Common;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -30,77 +29,176 @@ namespace Ve.Controllers
 
         [HttpPost("xuatVe")]
         [Authorize]
-        public async Task<IActionResult> CreateTicket([FromBody] ThongTinVe ticketInfo)
+        public async Task<IActionResult> CreateTicket([FromBody] List<ThongTinVe> ticketInfos)
         {
-            if (ticketInfo == null || string.IsNullOrEmpty(ticketInfo.ChangDi) ||
-                string.IsNullOrEmpty(ticketInfo.ChangVe) ||
-                string.IsNullOrEmpty(ticketInfo.MaDatChoHang) ||
-                ticketInfo.NgayGioBayDi == default ||
-                ticketInfo.NgayGioBayDen == default)
-            {
-                return BadRequest("Missing required fields");
-            }
+
 
             try
             {
-                ticketInfo = InitCreationInfo(ticketInfo);
+                for (int i = 0; i < ticketInfos.Count; i++)
+                {
+                    var ticketInfo = ticketInfos[i];
+                    if (ticketInfo == null || string.IsNullOrEmpty(ticketInfo.ChangDi) ||
+                    string.IsNullOrEmpty(ticketInfo.ChangVe) ||
+                    string.IsNullOrEmpty(ticketInfo.MaDatChoHang) ||
+                    ticketInfo.NgayGioBayDi == default ||
+                    ticketInfo.NgayGioBayDen == default)
+                    {
+                        return BadRequest("Missing required fields");
+                    }
+                    ticketInfo = InitCreationInfo(ticketInfo);
 
-                var card = await _repository.GetAsync<Card>(x => x.SoThe.Equals(ticketInfo.Card.SoThe));
-                if (card is null)
-                {
-                    ticketInfo.Card = InitCreationInfo(ticketInfo.Card);
-                    ticketInfo.Card.Id = Guid.NewGuid();
-                    ticketInfo.CardId = ticketInfo.Card.Id;
-                    _context.Cards.Add(ticketInfo.Card);
-                }
-                else
-                {
-                    ticketInfo.Card = card;
-                    ticketInfo.CardId = card.Id;
+                    var card = await _repository.GetAsync<Card>(x => x.SoThe.Equals(ticketInfo.Card.SoThe));
+                    if (card is null)
+                    {
+                        ticketInfo.Card = InitCreationInfo(ticketInfo.Card);
+                        ticketInfo.Card.Id = Guid.NewGuid();
+                        ticketInfo.CardId = ticketInfo.Card.Id;
+                        _context.Cards.Add(ticketInfo.Card);
+                    }
+                    else
+                    {
+                        ticketInfo.Card = card;
+                        ticketInfo.CardId = card.Id;
+                    }
+
+                    var ag = await _repository.GetAsync<AGCustomer>(x => x.SDT.Equals(ticketInfo.AGCustomer.SDT)
+                                                                                    && x.Mail.Equals(ticketInfo.AGCustomer.Mail)
+                                                                                    && x.TenAG.ToLower().Equals(ticketInfo.AGCustomer.TenAG.ToLower()));
+                    if (ag is null)
+                    {
+                        ticketInfo.AGCustomer = InitCreationInfo(ticketInfo.AGCustomer);
+                        ticketInfo.AGCustomer.Id = Guid.NewGuid();
+                        ticketInfo.AGCustomerId = ticketInfo.AGCustomer.Id;
+                        _context.AgCustomers.Add(ticketInfo.AGCustomer);
+                    }
+                    else
+                    {
+                        ticketInfo.AGCustomer = ag;
+                        ticketInfo.AGCustomerId = ag.Id;
+                    }
+
+                    var customer = await _repository.GetAsync<Customer>(x => x.GioiTinh.Equals(ticketInfo.Customer.GioiTinh)
+                                                                                    && x.TenKhachHang.ToLower().Equals(ticketInfo.Customer.TenKhachHang.ToLower()));
+                    if (customer is null)
+                    {
+                        ticketInfo.Customer = InitCreationInfo(ticketInfo.Customer);
+                        ticketInfo.Customer.Id = Guid.NewGuid();
+                        ticketInfo.CustomerId = ticketInfo.Customer.Id;
+                        _context.Customers.Add(ticketInfo.Customer);
+                    }
+                    else
+                    {
+                        ticketInfo.Customer = customer;
+                        ticketInfo.CustomerId = customer.Id;
+                    }
+
+                    var addTicket = ticketInfo.DeepCopy();
+                    addTicket.Customer = null;
+                    addTicket.AGCustomer = null;
+                    addTicket.Card = null;
+                    _context.ThongTinVes.Add(addTicket);
                 }
 
-                var ag = await _repository.GetAsync<AGCustomer>(x => x.SDT.Equals(ticketInfo.AGCustomer.SDT) 
-                                                                                && x.Mail.Equals(ticketInfo.AGCustomer.Mail)
-                                                                                && x.TenAG.ToLower().Equals(ticketInfo.AGCustomer.TenAG.ToLower()));
-                if (ag is null)
-                {
-                    ticketInfo.AGCustomer = InitCreationInfo(ticketInfo.AGCustomer);
-                    ticketInfo.AGCustomer.Id = Guid.NewGuid();
-                    ticketInfo.AGCustomerId = ticketInfo.AGCustomer.Id;
-                    _context.AgCustomers.Add(ticketInfo.AGCustomer);
-                }
-                else
-                {
-                    ticketInfo.AGCustomer = ag;
-                    ticketInfo.AGCustomerId = ag.Id;
-                }
-
-                var customer = await _repository.GetAsync<Customer>(x =>  x.GioiTinh.Equals(ticketInfo.Customer.GioiTinh)
-                                                                                && x.TenKhachHang.ToLower().Equals(ticketInfo.Customer.TenKhachHang.ToLower()));
-                if (customer is null)
-                {
-                    ticketInfo.Customer = InitCreationInfo(ticketInfo.Customer);
-                    ticketInfo.Customer.Id = Guid.NewGuid();
-                    ticketInfo.CustomerId = ticketInfo.Customer.Id;
-                    _context.Customers.Add(ticketInfo.Customer);
-                }
-                else
-                {
-                    ticketInfo.Customer = customer;
-                    ticketInfo.CustomerId = customer.Id;
-                }
-
-                var addTicket = ticketInfo.DeepCopy();
-                addTicket.Customer = null;
-                addTicket.AGCustomer = null;
-                addTicket.Card = null;
-                _context.ThongTinVes.Add(addTicket);
                 await _context.SaveChangesAsync();
-                return CreatedAtAction(nameof(CreateTicket), new { id = ticketInfo.Id }, ticketInfo);
+                return CreatedAtAction(nameof(CreateTicket), new { ticketInfos });
             }
             catch (Exception ex)
             {
                 return StatusCode(500, "Server error: " + ex.Message);
+            }
+        }
+
+        [HttpPut("xuatve")]
+        [Authorize]
+        public async Task<IActionResult> UpdateTicket([FromBody] List<ThongTinVe> ticketInfos)
+        {
+            try
+            {
+                for (int i = 0; i < ticketInfos.Count; i++)
+                {
+                    var ticketInfo = ticketInfos[i];
+                    if (ticketInfo == null || string.IsNullOrEmpty(ticketInfo.ChangDi) ||
+                    string.IsNullOrEmpty(ticketInfo.ChangVe) ||
+                    string.IsNullOrEmpty(ticketInfo.MaDatChoHang) ||
+                    ticketInfo.NgayGioBayDi == default ||
+                    ticketInfo.NgayGioBayDen == default)
+                    {
+                        return BadRequest("Missing required fields");
+                    }
+                    ticketInfo = InitUpdateInfo(ticketInfo);
+                    var card = await _repository.GetAsync<Card>(x => x.SoThe.Equals(ticketInfo.Card.SoThe));
+                    if (card is null)
+                    {
+                        ticketInfo.Card = InitCreationInfo(ticketInfo.Card);
+                        ticketInfo.Card.Id = Guid.NewGuid();
+                        ticketInfo.CardId = ticketInfo.Card.Id;
+                        _context.Cards.Add(ticketInfo.Card);
+                    }
+                    else
+                    {
+                        ticketInfo.Card = card;
+                        ticketInfo.CardId = card.Id;
+                    }
+
+                    var ag = await _repository.GetAsync<AGCustomer>(x => x.SDT.Equals(ticketInfo.AGCustomer.SDT)
+                                                                                    && x.Mail.Equals(ticketInfo.AGCustomer.Mail)
+                                                                                    && x.TenAG.ToLower().Equals(ticketInfo.AGCustomer.TenAG.ToLower()));
+                    if (ag is null)
+                    {
+                        ticketInfo.AGCustomer = InitCreationInfo(ticketInfo.AGCustomer);
+                        ticketInfo.AGCustomer.Id = Guid.NewGuid();
+                        ticketInfo.AGCustomerId = ticketInfo.AGCustomer.Id;
+                        _context.AgCustomers.Add(ticketInfo.AGCustomer);
+                    }
+                    else
+                    {
+                        ticketInfo.AGCustomer = ag;
+                        ticketInfo.AGCustomerId = ag.Id;
+                    }
+
+                    var customer = await _repository.GetAsync<Customer>(x => x.GioiTinh.Equals(ticketInfo.Customer.GioiTinh)
+                                                                                    && x.TenKhachHang.ToLower().Equals(ticketInfo.Customer.TenKhachHang.ToLower()));
+                    if (customer is null)
+                    {
+                        ticketInfo.Customer = InitCreationInfo(ticketInfo.Customer);
+                        ticketInfo.Customer.Id = Guid.NewGuid();
+                        ticketInfo.CustomerId = ticketInfo.Customer.Id;
+                        _context.Customers.Add(ticketInfo.Customer);
+                    }
+                    else
+                    {
+                        ticketInfo.Customer = customer;
+                        ticketInfo.CustomerId = customer.Id;
+                    }
+
+                    var updateTicket = ticketInfo.DeepCopy();
+                    updateTicket.Customer = null;
+                    updateTicket.AGCustomer = null;
+                    updateTicket.Card = null;
+                    _context.ThongTinVes.Update(updateTicket);
+                }
+                await _repository.SaveChangesAsync();
+                return Ok(ticketInfos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Server error:" + ex.ToString());
+            }
+        }
+
+        [HttpDelete("xuatve")]
+        public async Task<IActionResult> DeleteTicket([FromBody] List<Guid> deleteIds)
+        {
+            try
+            {
+                var deletes = await _repository.GetAllWithAsync<ThongTinVe>(x => deleteIds.Contains(x.Id));
+                await _repository.DeleteRangeAsync(deletes, true);
+                return Ok(deletes);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Server error:" + e.ToString());
             }
         }
 
@@ -142,6 +240,22 @@ namespace Ve.Controllers
             {
                 customer.ForEach(x => x.Id = Guid.NewGuid());
                 var result = await _repository.AddRangeAsync(customer, true);
+                return Ok(customer);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Server error: " + ex.Message);
+            }
+        }
+
+        [HttpDelete("ag")]
+        [Authorize]
+        public async Task<IActionResult> DeleteAg(List<Guid> customer)
+        {
+            try
+            {
+                var customers = await _repository.GetAllWithNoTrackingAsync<AGCustomer>(x => customer.Contains(x.Id)).ToListAsync();
+                var result = await _repository.DeleteRangeAsync(customers, true);
                 return Ok(customer);
             }
             catch (Exception ex)
@@ -196,6 +310,22 @@ namespace Ve.Controllers
             try
             {
                 var result = await _repository.GetAllWithAsync<Card>(x => x.SoThe.Contains(cardNumber));
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Server error: " + ex.Message);
+            }
+        }
+
+        [HttpDelete("card")]
+        [Authorize]
+        public async Task<IActionResult> GetCardInfo(List<Guid> cardIds)
+        {
+            try
+            {
+                var result = await _repository.GetAllWithAsync<Card>(x => cardIds.Contains(x.Id));
+                await _repository.DeleteRangeAsync(result, true);
                 return Ok(result);
             }
             catch (Exception ex)
